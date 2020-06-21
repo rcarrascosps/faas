711bb0c18b9ad1925fe6498b7620fef5d6ab77e9
Rolando Carrasco <rcarrasco@spsolutions.com.mx>
2020-06-21 10:05:56 -0500

    Change the bucketName env variable

--- rss-feeder/rssFeeder.js
+++ rss-feeder/rssFeeder.js
@@ -32,7 +32,7 @@ async function processRSS(rssURL, filename) {
         method: 'POST', uri: endpoint,
         json: true,
         body: {
-            "bucketname": process.env['bucketName']
+            "bucketname": process.env.bucketName
             , "filename": filename
             , "contents": JSON.stringify(result)
         },
